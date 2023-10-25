09ded84e9e3e9d79a1a569748fd7f9055ba0a2fb
adrickmalekian <adrickmalekian218@gmail.com>
2023-10-25 14:31:25 -0700

    added environment and setup express

--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -1,1 +1,18 @@
-console.log("hello world");
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+//GET endpoints
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+//running the server
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`);
+});
