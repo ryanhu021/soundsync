3d4266c0022dfcd08036113bb97962e0f5dc713a
RyanHu021 <ryanhu021@gmail.com>
2023-10-30 22:29:09 -0700

    add login route

--- packages/backend/src/routes/user-routes.ts
+++ packages/backend/src/routes/user-routes.ts
@@ -1,11 +1,17 @@
 import express from "express";
-import { register } from "../services/user-services";
+import { register, login } from "../services/user-services";
 
 const router = express.Router();
 
-router.post("/", (req, res) => {
+router.post("/register", (req, res) => {
   register(req.body.name, req.body.email, req.body.password)
     .then((user) => res.status(201).send(user))
+    .catch((error) => res.status(500).send(error));
+});
+
+router.post("/login", (req, res) => {
+  login(req.body.email, req.body.password)
+    .then((token) => res.status(200).send({ token }))
     .catch((error) => res.status(500).send(error));
 });
 
