50919d01a3fe0a9e04b301d55604499508766dcf
RyanHu021 <ryanhu021@gmail.com>
2023-11-01 14:09:47 -0700

    setup jwt auth and user routes

--- packages/backend/src/services/user-services.ts
+++ packages/backend/src/services/user-services.ts
@@ -1,6 +1,6 @@
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
 import { User } from "../models/user-model";
+import { generateToken } from "../util/auth";
 
 export const register = async (
   name: string,
@@ -8,7 +8,7 @@ export const register = async (
   password: string
 ): Promise<User> => {
   if ((await User.find({ email })).length > 0) {
-    Promise.reject("User already exists");
+    return Promise.reject("User already exists");
   }
   const hashedPassword = await bcrypt.hash(password, 10);
   const user = new User({
@@ -31,12 +31,5 @@ export const login = async (
   if (!(await bcrypt.compare(password, user.password))) {
     return Promise.reject("Invalid password");
   }
-  if (!process.env.JWT_SECRET) {
-    return Promise.reject("JWT secret not set");
-  }
-  return jwt.sign(
-    { id: user._id, name: user.name, email: user.email },
-    process.env.JWT_SECRET,
-    { expiresIn: "24h" }
-  );
+  return generateToken({ id: user._id });
 };
