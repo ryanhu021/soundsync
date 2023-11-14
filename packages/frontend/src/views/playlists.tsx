bb1716b889b5fed5e1fd30dbc14d2764dab99648
adrickmalekian <adrickmalekian218@gmail.com>
2023-11-13 16:10:24 -0800

    added the keys

--- packages/frontend/src/views/playlists.tsx
+++ packages/frontend/src/views/playlists.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+// import { useState, useEffect } from "react";
 // import { Link } from "react-router-dom";
 
 export default function Playlists() {
+  // const [songs, setSongs] = useState([]);
+
+  // useEffect(() => {
+  //   songs
+  // })
   return (
     <>
       <h1>Your Playlists</h1>
