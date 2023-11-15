a7957d047385116b3fffda874b014c7cc4f9231c
ktaschek <ktaschek@gmail.com>
2023-11-15 14:26:12 -0800

    delete unused

--- packages/frontend/src/views/playlists.tsx
+++ packages/frontend/src/views/playlists.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import DeezerLogin from "../components/deezer-signin";
 // import { Link } from "react-router-dom";
 
 export default function Playlists() {
   return (
     <>
       <h1>Playlists Page</h1>
-      <DeezerLogin />
     </>
   );
 }
