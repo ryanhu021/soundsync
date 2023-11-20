bac48da5e6dbf4bd4cc344fda6f9dcff4f00eaf3
ktaschek <ktaschek@gmail.com>
2023-11-19 17:33:34 -0800

    fix: navbar spacing and add: songcard, playlist card, addplaylistcard

---
+++ packages/frontend/src/components/add-playlist-card.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Card, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+function AddPlaylistCard() {
+  return (
+    <div style={{ padding: "0", margin: "0" }}>
+      <Card style={{ width: "10rem", height: "16.5rem" }}>
+        <Card.Body>
+          <Row>
+            <Link
+              to="/new_playlist_icon.png"
+              style={{ textDecoration: "none", padding: "0.2rem" }}
+            >
+              <Card.Img
+                variant="top"
+                src="/new_playlist_icon.png"
+                alt="new_playlist_icon"
+              />
+            </Link>
+          </Row>
+          <Row>
+            <div>
+              <h5
+                style={{
+                  fontWeight: "bold",
+                  marginBottom: "3%",
+                }}
+              >
+                New Playlist
+              </h5>
+              <p
+                style={{
+                  marginBottom: "5%",
+                }}
+              >
+                &nbsp;
+              </p>
+            </div>
+          </Row>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+}
+
+export default AddPlaylistCard;
