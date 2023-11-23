c39b08a5acfea28dc6240c31aea89ed801bc0a68
RyanHu021 <ryanhu021@gmail.com>
2023-11-22 16:53:45 -0800

    fix spotify export

--- packages/frontend/src/components/spotify-export.tsx
+++ packages/frontend/src/components/spotify-export.tsx
@@ -1,20 +1,9 @@
 import React from "react";
 import { Button } from "react-bootstrap";
 
-// const scopes = ["playlist-modify-public", "playlist-modify-private"];
-// const redirectUri = "http://localhost:3000";
-// const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
-// const state = "spotify_auth_state";
-// const showDialog = true;
-
-// const spotifyApi = new SpotifyWebApi({
-//   clientId: clientId,
-//   redirectUri: redirectUri,
-// });
-
 export default function SpotifyExport() {
   const handleSubmit = () => {
-    fetch("http://localhost:8000/song/auth/spotify", {
+    fetch(`${process.env.REACT_APP_SERVER_URL}/song/auth/spotify`, {
       method: "GET",
     })
       .then((res) => res.json())
