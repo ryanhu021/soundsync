63d7a1c63c31b23fb5bbedafb36a8cee2d7e967c
amalekia <amalekia@calpoly.edu>
2023-11-27 22:18:02 -0800

    added some stuff for export

--- packages/backend/src/routes/export-routes.ts
+++ packages/backend/src/routes/export-routes.ts
@@ -1,10 +1,27 @@
 import express from "express";
 import { AuthRequest, auth } from "../util/auth";
+import SpotifyWebApi from "spotify-web-api-node";
+import { User } from "../models/user-model";
 
 const router = express.Router();
+const redirectUri = `${process.env.CLIENT_URL}/auth/spotify/callback`;
 
 router.post("/spotify", auth, async (req: AuthRequest, res) => {
   res.send("spotify export");
+
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri,
+    clientId: process.env.SPOTIFY_CLIENT_ID || "",
+    clientSecret: process.env.SPOTIFY_CLIENT_SECRET || "",
+  });
+
+  if (req.user) {
+    spotifyApi.createPlaylist(req.user.name);
+  }
+  else {
+    throw new Error("Error accesing user playlist");
+  }
+
 });
 
 router.post("/deezer", auth, async (req: AuthRequest, res) => {
