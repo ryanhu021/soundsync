cda3759fc6bd978310595ab56cefb65eae938866
RyanHu021 <ryanhu021@gmail.com>
2023-12-04 20:09:08 -0800

    remove sameSite restriction for non-prod

--- packages/backend/src/services/user-services.ts
+++ packages/backend/src/services/user-services.ts
@@ -9,7 +9,7 @@ const serializeToken = (token: string, maxAge = JWT_EXPIRE) =>
   serialize("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    sameSite: "none",
     path: "/",
     maxAge,
   });
